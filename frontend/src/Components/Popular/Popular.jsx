4b2a410a09a05ef7968dd3ab5437d246dbee38c7
BharatRajputt <you@example.com>
2024-08-10 10:35:25 +0530

    http changes

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ export const Popular = () => {
    const [popularProducts,setPopularProducts] = useState([]);
 
    useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen').then((response)=>response.json()).then((data)=>setPopularProducts(data))
+    fetch('https://localhost:4000/popularinwomen').then((response)=>response.json()).then((data)=>setPopularProducts(data))
    
   
   },[])
